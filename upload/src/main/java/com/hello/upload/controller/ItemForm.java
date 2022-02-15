b62338aeb63c4fe39f382646c7cac7e3d4c9a459
bopi447 <bopi447@naver.com>
2022-02-15 14:56:42 +0900

    file upload, download

---
+++ upload/src/main/java/com/hello/upload/controller/ItemForm.java
@@ -0,0 +1,15 @@
+package com.hello.upload.controller;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class ItemForm {
+
+    private Long id;
+    private String itemName;
+    private MultipartFile attachFile;
+    private List<MultipartFile> imageFiles;
+}
