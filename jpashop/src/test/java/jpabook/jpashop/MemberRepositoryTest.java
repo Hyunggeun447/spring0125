a0f18d4766eb7ec9f60d8a2d4f385dc18efe6a0d
bopi447 <bopi447@naver.com>
2022-02-16 17:09:38 +0900

    entity class 개발

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,14 +1,12 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberRepositoryTest {
@@ -18,7 +16,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional //Test에 있으면 자동 롤백해줌
-    @Rollback(value = false) // 롤백하기 싫으면 false하면 롤백안됌
+//    @Rollback(value = false) // 롤백하기 싫으면 false하면 롤백안됌
     void testMember() throws Exception{
         //given
         Member member = new Member();
