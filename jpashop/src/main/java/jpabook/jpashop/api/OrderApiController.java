be4348048ab3eaa105527be130c95cec95d15776
bopi447 <bopi447@naver.com>
2022-02-24 13:13:28 +0900

    컬렉션 조회 성능최적화

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,10 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +23,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 
 /**
  * 컬렉션 조회 최적화 하기.
+ * 일단 엔티티를 기본으로 하고(이유 : 페치조인이든, 라이브러리 설정 변경이든 수정이 가장 쉽다.)
+ * 그럼에도 성능이 안나올경우 DTO방식으로
+ * 단건조회면 v4가 좋으나 기본적으로 v5가 제일 좋다.
+ * v6는 비추천(쿼리문은 1번에 끝나나 데이터 이동이 제일 많기때문에 코드복잡도에 비해 성능이 별로)
  */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public ResultApi ordersV1() {
@@ -101,6 +112,60 @@ public class OrderApiController {
         return new ResultApi(collect.size(), collect);
     }
 
+
+    /**
+     * 새로 OrderQueryRepository를 만듬.
+     * ToOne은 그냥 join으로 직접 조회해서 orderQueryDto를 만들고,
+     * loop 돌려서 orderQueryDto의 orderid로 직접 조회해서 orderItemQueryDto에 넣는다.
+     *
+     * 장점 : 코드가 단순. 특정 주문 한건만 조회하는거면(단건조회) 성능 제일 좋음.
+     * 단점 : N+1문제 발생
+     *
+     */
+    @GetMapping("/api/v4/orders")
+    public ResultApi ordersV4() {
+        List<OrderQueryDto> orderQueryDtos = orderQueryRepository.findOrderQueryDtos();
+        return new ResultApi(orderQueryDtos.size(), orderQueryDtos);
+    }
+
+
+    /**
+     * 쿼리문 = -> in 변환
+     * orderitem의 map화 O(1)로 만들기
+     * loop로 컬렉션 데이터 채우기
+     *
+     * 특징 : 코드 복잡. 여러 주문을 조회하는 것이면 이것을 사용해야함.
+     * 장점 : N+1 문제 해결. 제일 좋은듯.
+     *
+     */
+    @GetMapping("/api/v5/orders")
+    public ResultApi ordersV5() {
+        List<OrderQueryDto> result = orderQueryRepository.findAllByDto_optimization();
+
+        return new ResultApi(result.size(), result);
+    }
+
+    /**
+     * 1줄 쿼리로 요청 보내기.
+     * 장점 : 쿼리 1번
+     * 단점 : 페이징 불가능, 스팩도 안맞아서 변환작업 추가로 필요함(orderFlatDto -> orderQueryDto). 매우 높은 코드 복잡도
+     * 변환작업은 참고문서를 찾아봐야함.
+     * 비추천
+     */
+    @GetMapping("/api/v6/orders")
+    public ResultApi ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        List<OrderQueryDto> collect = flats.stream().collect(
+                        groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                                mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())))
+                .entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(),
+                        e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+
+        return new ResultApi(collect.size(), collect);
+    }
 
 
     @Data
