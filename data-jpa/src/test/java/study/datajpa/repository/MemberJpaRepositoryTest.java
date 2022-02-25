8447c724a29b7d338185ed3e1f6481462a34ce02
bopi447 <bopi447@naver.com>
2022-02-25 12:15:13 +0900

    공통 인터페이스 기능 JpaRepository<>

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -54,5 +56,71 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member);
 
     }
+
+    @Test
+    public void CRUD() throws Exception {
+
+        //given
+        Member memberA = new Member("memberA");
+        Member memberB = new Member("memberB");
+
+        memberJpaRepository.save(memberA);
+        memberJpaRepository.save(memberB);
+
+        //when
+
+        Member findMemberA = memberJpaRepository.findById(memberA.getId()).get();
+        Member findMemberB = memberJpaRepository.findById(memberB.getId()).get();
+
+        //then
+        assertThat(findMemberA).isEqualTo(memberA);
+        assertThat(findMemberB).isEqualTo(memberB);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(memberA);
+        memberJpaRepository.delete(memberB);
+
+        long countDelete = memberJpaRepository.count();
+        assertThat(countDelete).isEqualTo(0);
+    }
+
+    @Test
+    public void basicCRUD() throws Exception {
+
+        //given
+        Member memberA = new Member("memberA");
+        Member memberB = new Member("memberB");
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+
+        Member findMemberA = memberRepository.findById(memberA.getId()).get();
+        Member findMemberB = memberRepository.findById(memberB.getId()).get();
+
+        //then
+        assertThat(findMemberA).isEqualTo(memberA);
+        assertThat(findMemberB).isEqualTo(memberB);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(memberA);
+        memberRepository.delete(memberB);
+
+        long countDelete = memberRepository.count();
+        assertThat(countDelete).isEqualTo(0);
+
+    }
+
 
 }
