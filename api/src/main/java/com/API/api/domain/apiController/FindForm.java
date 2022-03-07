e45b1779cd74ddefca4c8465f9e6f93964c9c46c
bopi447 <bopi447@naver.com>
2022-03-07 22:10:20 +0900

    여러가지 API 통신방법 구현

---
+++ api/src/main/java/com/API/api/domain/apiController/FindForm.java
@@ -0,0 +1,9 @@
+package com.API.api.domain.apiController;
+
+import lombok.Data;
+
+@Data
+public class FindForm {
+
+    private String loginName;
+}
--- api/src/main/java/com/API/api/domain/apiController/MemberControllerApi.java
+++ api/src/main/java/com/API/api/domain/apiController/MemberControllerApi.java
@@ -4,10 +4,7 @@ import com.API.api.domain.dto.MemberDto;
 import com.API.api.domain.entity.Member;
 import com.API.api.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +12,10 @@ public class MemberControllerApi {
 
     private final MemberService memberService;
 
+
+    /**
+     * Body- text
+     */
     @PostMapping("/v1")
     public MemberDto findByLoginName(@RequestBody String loginName) {
         Member member = memberService.findByLoginName(loginName);
@@ -23,5 +24,37 @@ public class MemberControllerApi {
         return memberDto;
     }
 
+    /**
+     * 쿼리 파라미터 방식
+     */
+    @PostMapping("/v2")
+    public MemberDto findByLoginName2(@RequestParam String loginName) {
+        Member member = memberService.findByLoginName(loginName);
+        MemberDto memberDto = new MemberDto(member.getLoginName(), member.getMemberName());
+        return memberDto;
+    }
 
+    /**
+     * 쿼리 파라미터 방식
+     */
+    @PostMapping("/v3")
+    public MemberDto findByLoginName3(@ModelAttribute FindForm form) {
+        String loginName = form.getLoginName();
+        Member member = memberService.findByLoginName(loginName);
+        MemberDto memberDto = new MemberDto(member.getLoginName(), member.getMemberName());
+        return memberDto;
+    }
+
+    /**
+     * Body -JSON -> key:value
+     *
+     */
+    @PostMapping("/v4")
+    public MemberDto findByLoginName4(@RequestBody FindForm form) {
+        String loginName = form.getLoginName();
+        Member member = memberService.findByLoginName(loginName);
+        MemberDto memberDto = new MemberDto(member.getLoginName(), member.getMemberName());
+        return memberDto;
+
+    }
 }
