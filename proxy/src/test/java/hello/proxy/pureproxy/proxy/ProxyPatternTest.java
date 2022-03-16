02689f1984f7f95132cbdcccaac318e73fc7377a
bopi447 <bopi447@naver.com>
2022-03-16 20:20:36 +0900

    프록시 패턴, 캐시를 이용

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,40 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    public void noProxyTest() throws Exception {
+
+        //given
+
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+        client.execute();
+        client.execute();
+        client.execute();
+
+    }
+
+    /**
+     * 캐시를 이용
+     */
+    @Test
+    public void cacheProxyTest() throws Exception {
+
+        //given
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+
+    }
+}
