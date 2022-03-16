32a0b1bb7f0d0897ee3c510c72c967f96315b194
bopi447 <bopi447@naver.com>
2022-03-16 21:07:27 +0900

    decorator 패턴 예제 1~3

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,46 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    public void noDecorator() throws Exception {
+
+        //given
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+
+    }
+
+    @Test
+    public void decorator1() throws Exception {
+
+        //given
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(messageDecorator);
+
+        decoratorPatternClient.execute();
+
+    }
+    @Test
+    public void decorator2() throws Exception {
+
+        //given
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(timeDecorator);
+
+        decoratorPatternClient.execute();
+
+    }
+}
